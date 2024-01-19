3d7adb6c4b142f9e5c1ddf6ce4b286df56f9c6ed
MagameDev <47176588+MagameDev@users.noreply.github.com>
2024-01-20 02:28:25 +0300

    Update utils.hpp

--- utils.hpp
+++ utils.hpp
@@ -12,40 +12,4 @@ namespace Utils {
 			file.close();
 		}
 	}
-    bool GetFileCreationTime(const std::wstring& filePath, SYSTEMTIME* creationTime) {
-        HANDLE hFile = CreateFileW(filePath.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-
-        if (hFile == INVALID_HANDLE_VALUE) {
-            std::cerr << "Error opening file: " << GetLastError() << std::endl;
-            return false;
-        }
-
-        FILETIME ftCreate, ftAccess, ftWrite;
-
-        if (GetFileTime(hFile, &ftCreate, &ftAccess, &ftWrite)) {
-            FileTimeToSystemTime(&ftCreate, creationTime);
-            CloseHandle(hFile);
-            return true;
-        }
-
-        CloseHandle(hFile);
-        return false;
-    }
-    SYSTEMTIME GetSystemTime()
-    {
-        SYSTEMTIME currentTime;
-        GetSystemTime(&currentTime);
-        return currentTime;
-    }
-    bool SetSystemTime(const SYSTEMTIME& newSystemTime)
-    {
-        if (SetSystemTime(&newSystemTime))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
 }
